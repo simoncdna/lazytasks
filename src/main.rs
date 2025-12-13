f04d3714bf6365daf0f8276548ed346f8abea782
Simon Cardona <simon.cdna@proton.me>
2025-12-13 19:10:55 +0100

    feat: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()> {
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
