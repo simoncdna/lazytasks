6929091c6fb19b448f51f36019e52bad6350efde
Simon Cardona <simon.cdna@proton.me>
2025-12-26 12:18:37 +0100

    fix: copy

--- src/components/modals/archive_task.rs
+++ src/components/modals/archive_task.rs
@@ -7,15 +7,11 @@ use ratatui::{
 use crate::components::shared;
 
 pub fn render(frame: &mut Frame, selected_option: &mut ListState, is_archived: bool) {
-    let title = if is_archived {
-        "Unarchived"
-    } else {
-        "Archived"
-    };
-    let area = shared::modal::Modal::new(format!("{} confirmation", title))
+    let action_name = if is_archived { "Unarchive" } else { "Archive" };
+    let area = shared::modal::Modal::new(format!("{} confirmation", action_name))
         .height(4)
         .render(frame);
-    let list_items: Vec<ListItem> = vec![ListItem::from(title), ListItem::from("Cancel")];
+    let list_items: Vec<ListItem> = vec![ListItem::from(action_name), ListItem::from("Cancel")];
     let delete_options = List::new(list_items).highlight_style(
         Style::default()
             .bg(Color::Blue)
